87b9da6390ec631159e584064e182b138178cb22
Mahlatsek29 <124674856+Mahlatsek29@users.noreply.github.com>
2023-06-29 07:56:44 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Register from './Register';
+import { ToastContainer } from 'react-toastify';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import EmpListing from './components/EmpListing';
+import EmpCreate from './components/EmpCreate';
+import EmpDetail from './components/EmpDetail';
+import EmpEdit from './components/EmpEdit';
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ToastContainer theme='coloured' position='top-center'></ToastContainer>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/EmpPage' element={<EmpPage/>}></Route>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          {/* {<Route path="/emp" element={</>}/>} */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
+
+const EmpPage = () => {
+  return (
+    <div>
+      <h1>Employee Management Logging System</h1>
+      <EmpListing/>
+      <EmpCreate/>
+      <EmpDetail/>
+      <EmpEdit/>
+    </div>
+  );
+};
 
 export default App;
